c9d242e2d9e99349cc657f417f1163b1f8bb0b9f
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2023-09-28 23:54:34 -0300

    layout.js/overwriting classname to use manrope as the main font

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable}``${manrope.variable}`}>
+      <body className={`${inter.variable}``${manrope.variable} font-mr`}>
         {children}
       </body>
     </html>
