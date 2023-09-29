c1dfe48055cc836a18225cf512f4f54a340d7ef6
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2023-09-28 23:56:11 -0300

    layout.js/setting background colours into body classname

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable}``${manrope.variable} font-mr`}>
+      <body
+        className={`${inter.variable} ${manrope.variable} font-mr bg-light dark:bg-dark`}
+      >
         {children}
       </body>
     </html>
